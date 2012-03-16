a6cec91cd6c82522b3e592ae45469f4cf4103e20
Sam Xia <sxia@local.com>
2012-03-16 13:44:42 -0700

    another commit

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 		System.out.println("hi sam");
+		System.out.println("another change");
 	}
 }
